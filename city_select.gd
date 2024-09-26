3f0a22229942f55803ca53718e1ff38bb9c512ff
Natalie Weaver <natalie.tee.weaver@gmail.com>
2024-09-26 06:39:33 -0700

    Test

--- city_select.gd
+++ city_select.gd
@@ -17,3 +17,5 @@ func _on_check_button_toggled(toggled_on: bool) -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+#Test
